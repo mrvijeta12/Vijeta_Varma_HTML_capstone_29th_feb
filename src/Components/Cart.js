d191053942791a153d09b3a7a762a484f6da5141
vijeta <vijetavarma1@gmail.com>
2024-03-01 08:05:50 +0530

    save

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 import ProductContext from "../Context/ProductContext";
 import { useContext } from "react";
 import { MdCancel } from "react-icons/md";
-// import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+
 
 const Cart = () => {
-  const { cartItems, setCartItems,setForm } = useContext(ProductContext);
-
-  // console.log(cartItems);
+  const { cartItems, setCartItems, setForm, checkout } =
+    useContext(ProductContext);
   const [quantity, setQuantity] = useState(1);
+  const [total, setTotal] = useState();
  
 
   const inc = (productId) => {
-    setQuantity((prevCounts) => ({ ...prevCounts,[productId]: (prevCounts[productId] || 0) + 1,
+    setQuantity((prevCounts) => ({
+      ...prevCounts,
+      [productId]: (prevCounts[productId] || 1) + 1,
     }));
   };
   const dec = (productId) => {
@@ -22,46 +25,51 @@ const Cart = () => {
     }));
   };
 
-
   function remove(citemsId) {
     setCartItems((existItems) =>
       existItems.filter((item) => item.id !== citemsId)
     );
   }
 
-  function openForm(){
-    setForm(true)
-    
+  function calculateTotal() {
+    const total = cartItems.reduce((acc, citems) => {
+      return acc + ((quantity[citems.id] || 1) * citems.price);
+    }, 0);
+    setTotal(total);
   }
+
+ 
+  useEffect(() => {
+    calculateTotal();
+  }, [cartItems, quantity]);
+
 
 
   return (
-    <div className="cart-container">
-      {cartItems &&
-        cartItems.map((citems, index) => (
-          <div key={index} className="cartitems">
-            <button onClick={() => remove(citems.id)}>
-              <MdCancel />{" "}
-            </button>
-            <img src={citems.image} alt="" />
-            <p>{citems.title}</p>
-            <p>${citems.price}</p>
-            <button onClick={() => dec(citems.id)}>-</button>
-            <p> {quantity[citems.id] || 0}</p>
-            <button onClick={() => inc(citems.id)}>+</button>
-            <p>{(quantity[citems.id] * citems.price).toFixed(1)}</p>
+    <div>
+      <div className="cart-container">
+        {cartItems &&
+          cartItems.map((citems, index) => (
+            <div key={index} className="cartitems">
+              <button onClick={() => remove(citems.id)}>
+                <MdCancel />{" "}
+              </button>
+              <img src={citems.image} alt="" />
+              <p>{citems.title}</p>
+              <p>${citems.price}</p>
+              <button onClick={() => dec(citems.id)}>-</button>
+              <p> {quantity[citems.id] || 1}</p>
+              <button onClick={() => inc(citems.id)}>+</button>
+              <p>{((quantity[citems.id] || 1) * citems.price).toFixed(2)}</p>
 
-            
-          </div>
+            </div>
+          ))}
 
-        ))
-        }
-          <div>
-          {
-            cartItems && <button className="checkout" onClick={openForm}>Checkout</button>
-            
-          }
-          </div>
+              <div>
+              <p>${(total).toFixed(2)}</p>
+              </div>
+     
+      </div>
     </div>
   );
 };
